e5ccd09fd9ae704a62ba35af690ab8bf4a472c5b
Sono Willbrand <sono.willbrand@daubit.org>
2022-10-01 18:21:25 +0200

    view

--- tracy-web/src/components/Header.tsx
+++ tracy-web/src/components/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <Breadcrumb spacing="8px" separator={<ChevronRightIcon color="gray.500" />}>
       <BreadcrumbItem>
-        <BreadcrumbLink href="#">Home</BreadcrumbLink>
+        <BreadcrumbLink href="/">Home</BreadcrumbLink>
       </BreadcrumbItem>
       <BreadcrumbItem>
         <BreadcrumbLink href="/pools">Pools</BreadcrumbLink>
--- tracy-web/src/hooks/usePools.tsx
+++ tracy-web/src/hooks/usePools.tsx
@@ -1,14 +1,13 @@
 import { useQuery } from "react-query";
+import pools from "../assets/pools.json";
 
-const fetchPools = async (limit = 10) => {
-  const parsed = await (
-    await fetch("https://jsonplaceholder.typicode.com/posts")
-  ).json();
-  return parsed;
+const fetchPools = async () => {
+  //   const parsed = await (await fetch("http://localhost:3000/pools")).json();
+  return pools;
 };
 
-const usePools = (limit) => {
-  return useQuery(["posts", limit], () => fetchPosts(limit));
+const usePools = () => {
+  return useQuery("pools", fetchPools);
 };
 
 export { usePools, fetchPools };
