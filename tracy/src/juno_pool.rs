dbe9ffb98c14393b0e6651a5e61b2f8afe14965f
Sono Willbrand <sono.willbrand@daubit.org>
2022-10-01 10:02:17 +0200

    add info fetch

--- tracy/src/juno_pool.rs
+++ tracy/src/juno_pool.rs
@@ -1,8 +1,7 @@
-use serde::__private::from_utf8_lossy;
 use serde::{Deserialize, Serialize};
 
 use std::error::Error;
-use std::str;
+use std::str::{self, from_utf8};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct WasmContractResponse {
@@ -16,17 +15,27 @@ pub struct WasmContractRaw {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct JunoToken {
-    symbol: String,
-    amount: String,
-    address: String,
-    decimal: Option<u64>,
+    name: Option<String>,
+    symbol: Option<String>,
+    total_supply: Option<String>,
+    address: Option<String>,
+    decimals: Option<u64>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct JunoPool {
-    lp_address: String,
-    token1: JunoToken,
-    token2: JunoToken,
+    lp_token_address: String,
+    lp_token_supply: String,
+    token1_denom: JunoDenom,
+    token1_reserve: String,
+    token2_denom: JunoDenom,
+    token2_reserve: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct JunoDenom {
+    native: Option<String>,
+    cw20: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -35,21 +44,21 @@ pub struct WasmCodeContracts {
 }
 
 #[tokio::main]
-async fn get_query(url: &String, query: &Vec<(&str, &str)>) -> Result<String, Box<dyn Error>> {
+async fn get_query(url: &str, query: &Vec<(&str, &str)>) -> Result<String, Box<dyn Error>> {
     let client = reqwest::Client::new();
     let resp = client.get(url).query(query).send().await?.text().await?;
     Ok(resp)
 }
 
 pub fn get_contracts(api: &str, code_id: u64) -> Result<Vec<String>, Box<dyn Error>> {
-    let url = format!("{}/wasm/code/{}", &api.to_string(), code_id);
+    let url = format!("{}/wasm/code/{}/contracts", &api.to_string(), code_id);
     let res = get_query(&url, &vec![])?;
     let res = serde_json::from_str::<WasmCodeContracts>(res.as_str())?;
     Ok(res.result)
 }
 
 pub fn query_contract(
-    api: &String,
+    api: &str,
     contract_address: &str,
     msg: &str,
 ) -> Result<String, Box<dyn Error>> {
@@ -57,6 +66,43 @@ pub fn query_contract(
     let res = get_query(&url, &vec![("encoding", "base64")])?;
     let res = serde_json::from_str::<WasmContractResponse>(&res)?;
     Ok(res.result.smart)
+}
+
+pub fn get_token_info(api: &str, contract_address: &str) -> Result<JunoToken, Box<dyn Error>> {
+    let msg = "{ \"token_info\" : {} }";
+    let msg = base64::encode(msg);
+    let res = query_contract(api, contract_address, msg.as_str())?;
+    let decoded = base64::decode_config(res, base64::STANDARD)?;
+    let decoded = from_utf8(&decoded)?;
+    let mut token = serde_json::from_str::<JunoToken>(&decoded)?;
+    token.address = Some(contract_address.to_string());
+    Ok(token)
+}
+
+pub fn get_pool_info(api: &str, contract_address: &str) -> Result<JunoPool, Box<dyn Error>> {
+    let msg = "{ \"info\" : {} }";
+    let msg = base64::encode(msg);
+    let res = query_contract(api, contract_address, msg.as_str())?;
+    let decoded = base64::decode_config(res, base64::STANDARD)?;
+    let decoded = from_utf8(&decoded)?;
+    println!("{}", decoded);
+    let pool = serde_json::from_str::<JunoPool>(&decoded)?;
+    Ok(pool)
+}
+
+pub fn get_price_for(api: &str, contract_address: &str, for2: bool) -> Result<u64, Box<dyn Error>> {
+    let (method, arg): (&str, &str) = if for2 {
+        ("get_price_for_token2", "token1")
+    } else {
+        ("get_price_for_token1", "token2")
+    };
+    let msg = format!("{{ \"{}\" : \"{}\" }}", method, arg);
+    let msg = base64::encode(msg);
+    let res = query_contract(api, contract_address, msg.as_str())?;
+    let decoded = base64::decode_config(res, base64::STANDARD)?;
+    let decoded = from_utf8(&decoded)?;
+    let amount = serde_json::from_str::<u64>(&decoded)?;
+    Ok(amount)
 }
 
 // pub fn fetch_juno_pools(api: &str) -> Vec<JunoPool> {
--- tracy/src/main.rs
+++ tracy/src/main.rs
@@ -1,11 +1,13 @@
 use eyre::Result;
-use tracy::juno_pool::query_contract;
+use tracy::juno_pool::{get_token_info, query_contract, get_pool_info, get_contracts};
 use tracy::pools::fetch_osmosis_pools;
 
 fn main() -> Result<()> {
-    println!("{}", "oi");
     // fetch_osmosis_pools().await?;
-    let res = query_contract(&String::from("https://lcd-juno.itastakers.com"), &String::from("juno1vyvdyd70pz3yhnduzfhl098dk5pfpjl8nxmsrm6gmd3f7y5yrxvqw7e892"), &String::from("eyJ0b2tlbnMiOiB7Im93bmVyIjogImp1bm8xbmt5eWpqZzJ0bnpudThlMGZtc2E5ZW10NzY5Z2hzeDdoaG43a3UifSB9"));
-    println!("{}", res.unwrap());
+    let contract_address = "juno14mdhwtxfywk6tyyexqx5ju5qqtzzh8gj0g0c9rmj04ms8pc7xjkqxnsg2a";
+    let api = "https://lcd-juno.itastakers.com";
+    let res = get_pool_info(api, contract_address);
+    // let res = get_contracts(api, 16);
+    println!("{:?}", res.unwrap());
     Ok(())
 }
